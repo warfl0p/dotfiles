7e5641e29cad450cfe1625e43fdc98bcd80856da
warfl0p <matthias.warlop@hotmail.be>
2025-02-09 16:59:10 +0100

    add install

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install Zsh
+if ! command -v zsh &> /dev/null; then
+    echo "Installing Zsh..."
+    sudo apt update && sudo apt install -y zsh
+fi
+
+# Make Zsh default shell
+echo "Setting Zsh as default shell..."
+chsh -s $(which zsh)
+
+# Clone dotfiles
+DOTFILES_DIR="$HOME/.dotfiles"
+if [ ! -d "$DOTFILES_DIR" ]; then
+    echo "Cloning dotfiles..."
+    git clone https://github.com/YOUR_USERNAME/dotfiles.git "$DOTFILES_DIR"
+fi
+
+sudo apt install stow
+cd "$DOTFILES_DIR"
+stow zsh
+
+
+echo "Done! Restart your shell or log out and back in."
