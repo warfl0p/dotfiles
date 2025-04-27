6a36937214ee3d3f4556c80079ca5cd168b8c153
warfl0p <matthias.warlop@hotmail.be>
2025-04-27 23:25:57 +0200

    update gitconfig, fix path in stall.sh

--- install.sh
+++ install.sh
@@ -25,12 +25,19 @@ curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/bin
 oh-my-posh font install meslo
 
 
+# install gh and authorize
+if ! command -v gh &> /dev/null; then
+    echo "Installing GitHub CLI..."
+    sudo apt install gh
+    gh auth login
+fi
+
 sudo apt install stow
 cd "$DOTFILES_DIR"
 
 stow .
 # tmux theme and source config
-CATPUCCIN_DIR=="$HOME/.config/tmux/plugins/catppuccin/tmux"
+CATPUCCIN_DIR="$HOME/.config/tmux/plugins/catppuccin/tmux"
 if [ ! -d "$CATPUCCIN_DIR" ]; then
     mkdir -p ~/.config/tmux/plugins/catppuccin
     git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
@@ -50,13 +57,6 @@ fi
 # install fzf
 if ! command -v fzf >/dev/null 2>&1; then
   brew install fzf
-fi
-
-# install gh and authorize
-if ! command -v gh &> /dev/null; then
-    echo "Installing GitHub CLI..."
-    sudo apt install gh
-    gh auth login
 fi
 
 
