f5e66b6176794c1e5cdd278704f7381ad84612d2
warfl0p <matthias.warlop@hotmail.be>
2026-04-10 11:40:23 +0200

    omarchy update

--- nvim/.config/nvim/lua/plugins/python.lua
+++ nvim/.config/nvim/lua/plugins/python.lua
@@ -1,10 +1,29 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pyright = {},
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				pyright = {},
+			},
+		},
+	},
+
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				python = { "ruff_format" },
+			},
+		},
+	},
+
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"pyright",
+				"ruff",
+			},
+		},
+	},
 }
